eaff5d8183b78001bef5180d0b2141f15d65cef8
Zhuang, Rong <jojozhuang@gmail.com>
2018-04-08 19:53:33 -0700

    hashmap

---
+++ HashMap/src/johnny/datastructure/common/HashNode.java
@@ -0,0 +1,12 @@
+package johnny.datastructure.common;
+
+public class HashNode<K, V> {
+    public K key;
+    public V val;
+    public HashNode<K, V> next;
+    public HashNode(K key, V val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
+}
