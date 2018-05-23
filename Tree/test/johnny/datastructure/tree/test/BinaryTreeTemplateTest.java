bf4f1834329a71061ed5416b1d76b79f9dca91b1
Zhuang, Rong <jojozhuang@gmail.com>
2018-05-23 16:03:37 -0700

    template for binary tree traversal

--- Tree/test/johnny/datastructure/tree/test/BinaryTreeTemplateTest.java
+++ Tree/test/johnny/datastructure/tree/test/BinaryTreeTemplateTest.java
@@ -14,9 +14,9 @@ import org.junit.Test;
 import johnny.datastructure.common.ListUtil;
 import johnny.datastructure.common.TreeFactory;
 import johnny.datastructure.common.TreeNode;
-import johnny.datastructure.tree.BinaryTreeTraversal;
+import johnny.datastructure.tree.BinaryTreeTemplate;
 
-public class BinaryTreeTraversalTest {
+public class BinaryTreeTemplateTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -35,184 +35,158 @@ public class BinaryTreeTraversalTest {
     }
 
     @Test
-    public void testPreorderRecursion() {
-        System.out.println("testPreorderRecursion");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.preorderRecursion(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        List<Integer> result1 = bttObj.preorderRecursion(root1);
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
-        List<Integer> result2 = bttObj.preorderRecursion(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
-        List<Integer> result3 = bttObj.preorderRecursion(root3);
-        assertEquals(expect3, result3);
-    }
-
-    @Test
-    public void testInorderRecursion() {
-        System.out.println("testInorderRecursion");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.inorderRecursion(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> result1 = bttObj.inorderRecursion(root1);
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,1,3});
-        List<Integer> result2 = bttObj.inorderRecursion(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,3,2});
-        List<Integer> result3 = bttObj.inorderRecursion(root3);
-        assertEquals(expect3, result3);
-    }
-
-    @Test
-    public void testPostorderRecursion() {
-        System.out.println("testPostorderRecursion");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.postorderRecursion(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        List<Integer> result1 = bttObj.postorderRecursion(root1);
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,3,1});
-        List<Integer> result2 = bttObj.postorderRecursion(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{3,2,1});
-        List<Integer> result3 = bttObj.postorderRecursion(root3);
-        assertEquals(expect3, result3);
-    }
-
-    @Test
-    public void testPreorderIteration() {
-        System.out.println("testPreorderIteration");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.preorderIteration(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        List<Integer> result1 = bttObj.preorderIteration(root1);
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
-        List<Integer> result2 = bttObj.preorderIteration(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
-        List<Integer> result3 = bttObj.preorderIteration(root3);
-        assertEquals(expect3, result3);
-
-    }
-
-    @Test
-    public void testInorderIteration() {
-        System.out.println("testInorderIteration");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.inorderIteration(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> result1 = bttObj.inorderIteration(root1);
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,1,3});
-        List<Integer> result2 = bttObj.inorderIteration(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,3,2});
-        List<Integer> result3 = bttObj.inorderIteration(root3);
-        assertEquals(expect3, result3);
-    }
-
-    @Test
-    public void testPostorderIteration() {
-        System.out.println("testPostorderIteration");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<Integer> expResult = new ArrayList<Integer>();
-        List<Integer> result = bttObj.postorderIteration(root);
-        assertEquals(expResult, result);
-
-        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
-        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
-        List<Integer> result1 = bttObj.postorderIteration(root1);
-        assertEquals(expect1, result1);
-
-        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
-        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,3,1});
-        List<Integer> result2 = bttObj.postorderIteration(root2);
-        assertEquals(expect2, result2);
-
-        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
-        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{3,2,1});
-        List<Integer> result3 = bttObj.postorderIteration(root3);
-        assertEquals(expect3, result3);
-    }
-
-    @Test
-    public void testLevelOrder() {
-        System.out.println("testLevelOrder");
-        TreeNode root = null;
-        BinaryTreeTraversal bttObj = new BinaryTreeTraversal();
-        List<List<Integer>> expResult = new ArrayList<List<Integer>>();
-        List<List<Integer>> result = bttObj.levelOrder(root);
-        assertEquals(expResult, result);
+    public void testPreorderTraversal() {
+        System.out.println("testPreorderTraversal");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
         
-        TreeNode p2 = new TreeNode(1);
-        List<List<Integer>> ret2 = ListUtil.buildIntegerList2(new Integer[][] {{1}});
-        assertEquals(ret2, bttObj.levelOrder(p2));
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.preorderTraversal(root);
+        assertEquals(expResult, result);
 
-        TreeNode p3 = TreeFactory.createInstance(new String[] {"1","2","#"});
-        List<List<Integer>> ret3 = ListUtil.buildIntegerList2(new Integer[][] {{1},{2}});
-        assertEquals(ret3, bttObj.levelOrder(p3));
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        List<Integer> result1 = bttObj.preorderTraversal(root1);
+        assertEquals(expect1, result1);
 
-        TreeNode p4 = TreeFactory.createInstance(new String[] {"1","2","3"});
-        List<List<Integer>> ret4 = ListUtil.buildIntegerList2(new Integer[][] {{1},{2,3}});
-        assertEquals(ret4, bttObj.levelOrder(p4));
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
+        List<Integer> result2 = bttObj.preorderTraversal(root2);
+        assertEquals(expect2, result2);
 
-        TreeNode p5 = TreeFactory.createInstance(new String[] {"1","#","3","#","5"});
-        List<List<Integer>> ret5 = ListUtil.buildIntegerList2(new Integer[][] {{1},{3},{5}});
-        assertEquals(ret5, bttObj.levelOrder(p5));
-
-        TreeNode p6 = TreeFactory.createInstance(new String[] {"3","9","20","#","#","15","7"});
-        List<List<Integer>> ret6 = ListUtil.buildIntegerList2(new Integer[][] {{3},{9,20},{15,7}});
-        assertEquals(ret6, bttObj.levelOrder(p6));
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
+        List<Integer> result3 = bttObj.preorderTraversal(root3);
+        assertEquals(expect3, result3);
     }
 
+    @Test
+    public void testInorderTraversal() {
+        System.out.println("testInorderTraversal");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
+        
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.inorderTraversal(root);
+        assertEquals(expResult, result);
+
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> result1 = bttObj.inorderTraversal(root1);
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        assertEquals(expect1, result1);
+
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,1,3});
+        List<Integer> result2 = bttObj.inorderTraversal(root2);
+        assertEquals(expect2, result2);
+
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,3,2});
+        List<Integer> result3 = bttObj.inorderTraversal(root3);
+        assertEquals(expect3, result3);
+    }
+
+    @Test
+    public void testPostorderTraversal() {
+        System.out.println("testPostorderTraversal");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
+
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.postorderTraversal(root);
+        assertEquals(expResult, result);
+
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        List<Integer> result1 = bttObj.postorderTraversal(root1);
+        assertEquals(expect1, result1);
+
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{2,3,1});
+        List<Integer> result2 = bttObj.postorderTraversal(root2);
+        assertEquals(expect2, result2);
+
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{3,2,1});
+        List<Integer> result3 = bttObj.postorderTraversal(root3);
+        assertEquals(expect3, result3);
+    }
+    
+    @Test
+    public void testPreorderTraversal2() {
+        System.out.println("testPreorderTraversal2");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
+        
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.preorderTraversal2(root);
+        assertEquals(expResult, result);
+
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        List<Integer> result1 = bttObj.preorderTraversal2(root1);
+        assertEquals(expect1, result1);
+
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{1,3,2});
+        List<Integer> result2 = bttObj.preorderTraversal2(root2);
+        assertEquals(expect2, result2);
+
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{1,2,3});
+        List<Integer> result3 = bttObj.preorderTraversal2(root3);
+        assertEquals(expect3, result3);
+    }
+
+    @Test
+    public void testInorderTraversal2() {
+        System.out.println("testInorderTraversal");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
+        
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.inorderTraversal2(root);
+        assertEquals(expResult, result);
+
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> result1 = bttObj.inorderTraversal2(root1);
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        assertEquals(expect1, result1);
+
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{3,1,2});
+        List<Integer> result2 = bttObj.inorderTraversal2(root2);
+        assertEquals(expect2, result2);
+
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{2,3,1});
+        List<Integer> result3 = bttObj.inorderTraversal2(root3);
+        assertEquals(expect3, result3);
+    }
+
+    @Test
+    public void testPostorderTraversal2() {
+        System.out.println("testPostorderTraversal2");
+        BinaryTreeTemplate bttObj = new BinaryTreeTemplate();
+
+        TreeNode root = null;
+        List<Integer> expResult = new ArrayList<Integer>();
+        List<Integer> result = bttObj.postorderTraversal2(root);
+        assertEquals(expResult, result);
+
+        TreeNode root1 = TreeFactory.createInstance(new String[]{"1"});
+        List<Integer> expect1 = ListUtil.buildIntegerList(new Integer[]{1});
+        List<Integer> result1 = bttObj.postorderTraversal2(root1);
+        assertEquals(expect1, result1);
+
+        TreeNode root2 = TreeFactory.createInstance(new String[]{"1","2","3"});
+        List<Integer> expect2 = ListUtil.buildIntegerList(new Integer[]{3,2,1});
+        List<Integer> result2 = bttObj.postorderTraversal2(root2);
+        assertEquals(expect2, result2);
+
+        TreeNode root3 = TreeFactory.createInstance(new String[]{"1","#","2","3","#"});
+        List<Integer> expect3 = ListUtil.buildIntegerList(new Integer[]{3,2,1});
+        List<Integer> result3 = bttObj.postorderTraversal2(root3);
+        assertEquals(expect3, result3);
+    }
 }
