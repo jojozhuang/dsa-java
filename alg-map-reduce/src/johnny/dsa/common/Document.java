395f27fab0695bdf790dd62be49cbd75b1e5399a
Zhuang, Rong <jojozhuang@gmail.com>
2018-10-05 15:39:52 -0700

    new alg-map-reduce

---
+++ alg-map-reduce/src/johnny/dsa/common/Document.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package johnny.dsa.common;
+
+/**
+ *
+ * @author Johnny
+ */
+public class Document {
+    public int id;
+    public String content;
+}
