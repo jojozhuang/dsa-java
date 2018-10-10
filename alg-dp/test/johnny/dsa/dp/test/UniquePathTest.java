bb583dd49049ad8c88638b2e7ef7241e4c0767fc
Zhuang, Rong <jojozhuang@gmail.com>
2018-10-09 21:01:09 -0700

    alg-dp

---
+++ alg-dp/test/johnny/dsa/dp/test/UniquePathTest.java
@@ -0,0 +1,82 @@
+package johnny.dsa.dp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import johnny.dsa.dp.UniquePath;
+
+public class UniquePathTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testUniquePathMatrix() {
+        System.out.println("testUniquePathMatrix");
+        UniquePath instance = new UniquePath();
+        
+        assertEquals(0, instance.uniquePathMatrix(0, 0));
+        assertEquals(1, instance.uniquePathMatrix(1, 1));
+        assertEquals(1, instance.uniquePathMatrix(3, 1));
+        assertEquals(1, instance.uniquePathMatrix(1, 4));
+        assertEquals(2, instance.uniquePathMatrix(2, 2));
+        assertEquals(3, instance.uniquePathMatrix(3, 2));
+        assertEquals(3, instance.uniquePathMatrix(2, 3));
+        assertEquals(6, instance.uniquePathMatrix(3, 3));
+        assertEquals(86493225, instance.uniquePathMatrix(19, 13));
+        
+        assertEquals(0, instance.uniquePathMatrix2(0, 0));
+        assertEquals(1, instance.uniquePathMatrix2(1, 1));
+        assertEquals(1, instance.uniquePathMatrix2(3, 1));
+        assertEquals(1, instance.uniquePathMatrix2(1, 4));
+        assertEquals(2, instance.uniquePathMatrix2(2, 2));
+        assertEquals(3, instance.uniquePathMatrix2(3, 2));
+        assertEquals(3, instance.uniquePathMatrix2(2, 3));
+        assertEquals(6, instance.uniquePathMatrix2(3, 3));
+        assertEquals(86493225, instance.uniquePathMatrix2(19, 13));
+    }
+    
+    @Test
+    public void testUniquePathArray() {
+        System.out.println("testUniquePathArray");
+        UniquePath instance = new UniquePath();
+        
+        assertEquals(0, instance.uniquePathArray(0, 0));
+        assertEquals(1, instance.uniquePathArray(1, 1));
+        assertEquals(1, instance.uniquePathArray(3, 1));
+        assertEquals(1, instance.uniquePathArray(1, 4));
+        assertEquals(2, instance.uniquePathArray(2, 2));
+        assertEquals(3, instance.uniquePathArray(3, 2));
+        assertEquals(3, instance.uniquePathArray(2, 3));
+        assertEquals(6, instance.uniquePathArray(3, 3));
+        assertEquals(86493225, instance.uniquePathArray(19, 13));
+        
+        assertEquals(0, instance.uniquePath(0, 0));
+        assertEquals(1, instance.uniquePath(1, 1));
+        assertEquals(1, instance.uniquePath(3, 1));
+        assertEquals(1, instance.uniquePath(1, 4));
+        assertEquals(2, instance.uniquePath(2, 2));
+        assertEquals(3, instance.uniquePath(3, 2));
+        assertEquals(3, instance.uniquePath(2, 3));
+        assertEquals(6, instance.uniquePath(3, 3));
+        assertEquals(86493225, instance.uniquePath(19, 13));
+    }
+}
