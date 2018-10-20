1743bbab0d3c5b6a2120724e4628ea7fae492401
Zhuang, Rong <jojozhuang@gmail.com>
2018-10-19 20:48:47 -0700

    update test case of sqrt decomposition

--- alg-sqrt-decomposition/test/johnny/dsa/sqrtdecomposition/test/RangeSearchSumTest.java
+++ alg-sqrt-decomposition/test/johnny/dsa/sqrtdecomposition/test/RangeSearchSumTest.java
@@ -31,41 +31,41 @@ public class RangeSearchSumTest {
     @Test
     public void testPerfectSquare() {
         System.out.println("testPerfectSquare");
-        RangeSearchSum sd = new RangeSearchSum(new int[] {3,2,1,5,6,10,3,9,8});
-        // {3,2,1,5,6,10,3,9,8}, length = 9
-        assertEquals(5, sd.query(0, 1));
-        assertEquals(6, sd.query(0, 2));
+        RangeSearchSum sd = new RangeSearchSum(new int[] {3,5,1,5,6,10,4,2,8});
+        // {3,5,1,5,6,10,4,2,8}, length = 9
+        assertEquals(8, sd.query(0, 1));
+        assertEquals(9, sd.query(0, 2));
         assertEquals(21, sd.query(3, 5));
-        assertEquals(20, sd.query(6, 8));
-        assertEquals(33, sd.query(3, 7));
+        assertEquals(14, sd.query(6, 8));
+        assertEquals(27, sd.query(3, 7));
+        assertEquals(28, sd.query(2, 7));
+        assertEquals(44, sd.query(0, 8));
+        sd.update(2, 7); // {3,5,7,5,6,10,4,2,8}
+        assertEquals(15, sd.query(0, 2));
         assertEquals(34, sd.query(2, 7));
-        sd.update(2, 7); // {3,2,7,5,6,10,3,9,8}
-        assertEquals(12, sd.query(0, 2));
-        assertEquals(40, sd.query(2, 7));
-        assertEquals(12, sd.query(6, 7));
-        assertEquals(17, sd.query(7, 8));
-        sd.update(7, 12); // {3,2,7,5,6,10,3,12,8}
-        assertEquals(15, sd.query(6, 7));
+        assertEquals(6, sd.query(6, 7));
+        assertEquals(10, sd.query(7, 8));
+        sd.update(7, 12); // {3,5,7,5,6,10,4,12,8}
+        assertEquals(16, sd.query(6, 7));
         assertEquals(20, sd.query(7, 8));
-        assertEquals(56, sd.query(0, 8));
+        assertEquals(60, sd.query(0, 8));
     }
     
     @Test
     public void testNonPerfectSquare() {
         System.out.println("testNonSqrtDecomposition");
-        RangeSearchSum sd = new RangeSearchSum(new int[] {3,2,1,5,6,10,3,9,8,4,2});
-        //{3,2,1,5,6,10,3,9,8,4,2,0,0,0,0,0}, length = 16
-        assertEquals(5, sd.query(0, 1));
-        assertEquals(6, sd.query(0, 2));
+        RangeSearchSum sd = new RangeSearchSum(new int[] {3,5,1,5,6,10,4,2,8,9,7});
+        //{3,5,1,5,6,10,4,2,8,9,7,0,0,0,0,0}, length = 16
+        assertEquals(8, sd.query(0, 1));
+        assertEquals(9, sd.query(0, 2));
         assertEquals(21, sd.query(3, 5));
-        assertEquals(20, sd.query(6, 8));
-        assertEquals(34, sd.query(2, 7));
-        assertEquals(33, sd.query(3, 7));
-        assertEquals(6, sd.query(9, 10));
-        assertEquals(34, sd.query(2, 7));
-        sd.update(2, 7); // {3,2,7,5,6,10,3,9,8,4,2}
-        assertEquals(12, sd.query(0, 2));
-        assertEquals(54, sd.query(2, 10));
-        assertEquals(59, sd.query(0, 10));
+        assertEquals(14, sd.query(6, 8));
+        assertEquals(27, sd.query(3, 7));
+        assertEquals(28, sd.query(2, 7));
+        assertEquals(16, sd.query(9, 10));
+        sd.update(2, 7); // {3,5,7,5,6,10,4,2,8,9,7,0,0,0,0,0}
+        assertEquals(15, sd.query(0, 2));
+        assertEquals(58, sd.query(2, 10));
+        assertEquals(66, sd.query(0, 10));
     }
 }
