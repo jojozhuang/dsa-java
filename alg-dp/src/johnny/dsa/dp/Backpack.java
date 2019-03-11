765acc85f1a32cb6abc9ba2d37aca4f6a19df771
Zhuang, Rong <r.zhuang@sap.com>
2019-03-11 16:09:07 -0700

    dp, back pack

---
+++ alg-dp/src/johnny/dsa/dp/Backpack.java
@@ -0,0 +1,44 @@
+package johnny.dsa.dp;
+
+/*
+ * Given N items, A[i] is the weight of the i-th item. And given a backpack with capacity W, maximize the total weight
+ * that the backpack can be filled. Each item can be use 0 or 1 time.
+ */
+public class Backpack {
+    public int backPack(int[] A, int w) {
+        // write your code here
+        boolean f[][] = new boolean[A.length + 1][w + 1];
+        f[0][0] = true;
+
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 0; j <= w; j++) {
+                if (j >= A[i-1] && f[i-1][j - A[i-1]]) {
+                    f[i][j] = true;
+                } else {
+                    f[i][j] = f[i - 1][j];
+                }
+            }
+        }
+
+        for (int i = w; i >= 0; i--) {
+            if (f[A.length][i]) {
+                return i;
+            }
+        }
+
+        return 0;
+
+    }
+
+    public int backPack2(int[] A, int w) {
+        // maximum weight can be filled for the every capacity
+        int[] dp = new int[w + 1];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = w; j >= A[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - A[i]] + A[i]);
+            }
+        }
+        return dp[w];
+    }
+}
---
+++ alg-dp/src/johnny/dsa/dp/Backpack2.java
@@ -0,0 +1,26 @@
+package johnny.dsa.dp;
+
+/*
+ * Given N items, A[i] is the weight of the i-th item and V[i] is the value of the i-th item. Given a backpack with
+ * capacity w, maximize the total value. Each item can be use 0 or 1 time.
+ */
+public class Backpack2 {
+    public int backPack(int[] A, int[] V, int w) {
+        // Max value achieved by using the first i items and total weight is exact j.
+        int[][] dp = new int[A.length + 1][w + 1];
+
+        for (int i = 0; i <= A.length; i++) {
+            for (int j = 0; j <= w; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (A[i - 1] <= j)
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[A.length][w];
+    }
+}
