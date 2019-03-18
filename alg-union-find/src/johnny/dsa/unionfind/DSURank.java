a66c4383f55d867b11487fee95f125d1ba4d7d2d
Zhuang, Rong <r.zhuang@sap.com>
2019-03-18 14:23:45 -0700

    dsa, union and find

--- alg-union-find/src/johnny/dsa/unionfind/DSURank.java
+++ alg-union-find/src/johnny/dsa/unionfind/DSURank.java
@@ -1,57 +1,55 @@
 package johnny.dsa.unionfind;
 
-public class DisjointSetOptimized {
+public class DSURank { // Disjoint Set Union with Rank
     public int[] parents;
     public int[] rank;
     
-    public DisjointSetOptimized(int size)
-    {
+    public DSURank(int size) {
         parents = new int[size];
-        rank = new int[size];
         for (int i = 0; i < parents.length; i++) {
             // Initially, all elements are in their own set.
             parents[i] = i;
         }
+        rank = new int[size];
     }
 
     // Path Compression
     public int find(int i) {
-        if (parents[i] == i) {
-            // Then i is the representative of this set
-            return i;
-        } else {
-            // Recursively find the representative.
-            int result = find(parents[i]);
-
-            // Change the parent during traversal
-            parents[i] = result;
-
-            // Return the result
-            return result;
+        /*if (parents[i] != i) {
+            parents[i] = find(parents[i]);
         }
+        return parents[i];
+        */
+
+        // without recursion
+        while (parents[i] != i) {
+            parents[i] = parents[parents[i]];
+            i = parents[i];
+        }
+        return parents[i];
     }
 
     // Union by rank
     public void union(int i, int j) {
-        int root1 = find(i);
-        int root2 = find(j);
-        if (root1 == root2) {
+        int p1 = find(i);
+        int p2 = find(j);
+        if (p1 == p2) {
             return;
         }
         
         // If root1’s rank is less than root2’s rank
-        if (rank[root1] < rank[root2]) {
+        if (rank[p1] < rank[p2]) {
             // Then move root1 under root2
-            parents[root1] = root2;
+            parents[p1] = p2;
         // If root1’s rank is larger than root2’s rank
-        } else if (rank[root1] > rank[root2]) {
+        } else if (rank[p1] > rank[p2]) {
             // Then move root2 under root1
-            parents[root2] = root1;
+            parents[p2] = p1;
         // if ranks are the same 
         } else { 
             // Then move root1 under root2 (doesn't matter which one goes where) 
-            parents[root1] = root2;
-            rank[root2]++;
+            parents[p1] = p2;
+            rank[p2]++;
         }
     }
 }
