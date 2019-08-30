9c1d9bf7341c08d9a977d2ae59421028be804635
Zhuang, Rong <jojozhuang@gmail.com>
2019-08-30 14:46:18 -0700

    add monotic queue

--- ds-monotonic-queue/src/johnny/dsa/monotonicqueue/FirstSmallerMonotonicQueue.java
+++ ds-monotonic-queue/src/johnny/dsa/monotonicqueue/FirstSmallerMonotonicQueue.java
@@ -1,57 +1,51 @@
 package johnny.dsa.monotonicqueue;
 
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class FirstSmallerMonotonicQueue {
-    // input:  [ 5, 3,  1, 2, 4]
-    // output: [-1,-1, -1, 1, 2]
+    // input:  [5, 3, 1, 2, 4]
+    // output: [[-1,-1, -1, 1, 2], [3, 1, -1, -1, -1]]
     // O(n)
-    public int[] firstSmallerLeft(int[] nums) {
-        int[] ans = new int[nums.length];
-        Stack<Integer> stack = new Stack<>(); // increasing stack
-        for (int i = 0; i < nums.length; i++) { // left to right
-            while (!stack.isEmpty()) {
-                if (stack.peek() >= nums[i]) {
-                    stack.pop();
-                } else {
-                    break;
-                }
+    public int[][] firstSmaller(int[] nums) {
+        int[] leftSmaller = new int[nums.length];
+        int[] rightSmaller = new int[nums.length];
+        Arrays.fill(leftSmaller, -1);
+        Arrays.fill(rightSmaller, -1);
+        Deque<Integer> queue = new LinkedList<>(); // increasing queue
+        for (int i = 0; i < nums.length; i++) {    // left to right
+            while (!queue.isEmpty() && nums[queue.peekLast()] >= nums[i]) {
+                rightSmaller[queue.pollLast()] = nums[i];
             }
-            if (stack.isEmpty()) {
-                stack.push(nums[i]);
-                ans[i] = -1;
-            } else {
-                ans[i] = stack.peek();
-                stack.push(nums[i]);
+            if (!queue.isEmpty()) {
+                leftSmaller[i] = nums[queue.peekLast()];
             }
+            queue.offerLast(i);
         }
 
-        return ans;
+        return new int[][]{leftSmaller, rightSmaller};
     }
 
-    // input:  [5, 3,  1,  2,  4]
-    // output: [3, 1, -1, -1, -1]
+    // input:  [5, 3, 1, 2, 4]
+    // output: [[-1, 5, 3, 3, 5], [-1, 4, 2, 4, -1]]
     // O(n)
-    public int[] firstSmallerRight(int[] nums) {
-        int[] ans = new int[nums.length];
-        Stack<Integer> stack = new Stack<>(); // increasing stack
-        for (int i = nums.length - 1; i >= 0; i--) { // right to left
-            while (!stack.isEmpty()) {
-                if (stack.peek() >= nums[i]) {
-                    stack.pop();
-                } else {
-                    break;
-                }
+    public int[][] firstLarger(int[] nums) {
+        int[] leftLarger = new int[nums.length];
+        int[] rightLarger = new int[nums.length];
+        Arrays.fill(leftLarger, -1);
+        Arrays.fill(rightLarger, -1);
+        Deque<Integer> queue = new LinkedList<>(); // decreasing queue
+        for (int i = 0; i < nums.length; i++) {    // left to right
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                rightLarger[queue.pollLast()] = nums[i];
             }
-            if (stack.isEmpty()) {
-                stack.push(nums[i]);
-                ans[i] = -1;
-            } else {
-                ans[i] = stack.peek();
-                stack.push(nums[i]);
+            if (!queue.isEmpty()) {
+                leftLarger[i] = nums[queue.peekLast()];
             }
+            queue.offerLast(i);
         }
 
-        return ans;
+        return new int[][]{leftLarger, rightLarger};
     }
 }
--- ds-monotonic-queue/test/johnny/dsa/monotonicqueue/test/FirstSmallerMonotonicQueueTest.java
+++ ds-monotonic-queue/test/johnny/dsa/monotonicqueue/test/FirstSmallerMonotonicQueueTest.java
@@ -10,15 +10,25 @@ class FirstSmallerMonotonicQueueTest {
     @Test
     public void testFirstSmallerLeft() {
         FirstSmallerMonotonicQueue instance = new FirstSmallerMonotonicQueue();
-        assertArrayEquals(new int[]{-1,-1,-1,1,2}, instance.firstSmallerLeft(new int[] {5,3,1,2,4}));
-        assertArrayEquals(new int[]{-1,-1,-1,1,2,4,-1,0,0,1}, instance.firstSmallerLeft(new int[] {5,3,1,2,4,8,0,7,1,3}));
+        int[][] result1 = instance.firstSmaller(new int[] {5,3,1,2,4});
+        assertArrayEquals(new int[]{-1,-1,-1,1,2}, result1[0]);
+        assertArrayEquals(new int[]{3,1,-1,-1,-1}, result1[1]);
+
+        int[][] result2 = instance.firstSmaller(new int[] {5,3,1,2,4,8,0,7,1,3});
+        assertArrayEquals(new int[]{-1,-1,-1,1,2,4,-1,0,0,1}, result2[0]);
+        assertArrayEquals(new int[]{3,1,0,0,0,0,-1,1,-1,-1}, result2[1]);
     }
 
     @Test
     public void testFirstSmallerRight() {
         FirstSmallerMonotonicQueue instance = new FirstSmallerMonotonicQueue();
-        assertArrayEquals(new int[]{3,1,-1,-1,-1}, instance.firstSmallerRight(new int[] {5,3,1,2,4}));
-        assertArrayEquals(new int[]{3,1,0,0,0,0,-1,1,-1,-1}, instance.firstSmallerRight(new int[] {5,3,1,2,4,8,0,7,1,3}));
+        int[][] result1 = instance.firstLarger(new int[] {5,3,1,2,4});
+        assertArrayEquals(new int[]{-1,5,3,3,5}, result1[0]);
+        assertArrayEquals(new int[]{-1,4,2,4,-1}, result1[1]);
+
+        int[][] result2 = instance.firstLarger(new int[] {5,3,1,2,4,8,0,7,1,3});
+        assertArrayEquals(new int[]{-1,5,3,3,5,-1,8,8,7,7}, result2[0]);
+        assertArrayEquals(new int[]{8,4,2,4,8,-1,7,-1,3,-1}, result2[1]);
     }
 
 }
